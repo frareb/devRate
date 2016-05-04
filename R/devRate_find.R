e82d4d506cd3ede645d7f603dd9727d5f0c8ec13
Francois Rebaudo <francois.rebaudo@ird.fr>
2016-05-04 13:22:12 -0400

    main function

--- R/devRate_find.R
+++ R/devRate_find.R
@@ -1,7 +1,29 @@
 #' Find models for species
 #'
-devRateFind <- function(ordersp = "", family = "", species = ""){
-
+devRateFind <- function(orderSP = "", familySP = "", species = ""){
+  # getVarName <- function (myVar, name=deparse(substitute(myVar))) {
+  #   return(name)
+  # }
+  for(i in names(devRateEqList)){
+    eq <- get(i)
+    if(orderSP != "" & familySP == "" & species == ""){
+      if(orderSP %in% eq$startVal[,"ordersp"] == TRUE){
+        cat("\n", strwrap(x = paste0(eq$name, ": ", eq$refShort, " [", i, "]"), width = 80))
+      }
+    }
+    if(familySP != "" & species == ""){
+      if(orderSP %in% eq$startVal[,"familysp"] == TRUE){
+        cat("\n", strwrap(x = paste0(eq$name, ": ", eq$refShort, " [", i, "]"), width = 80))
+      }
+    }
+    if(species != ""){
+      if(orderSP %in% eq$startVal[,"sp"] == TRUE){
+        cat("\n", strwrap(x = paste0(eq$name, ": ", eq$refShort, " [", i, "]"), width = 80))
+      }
+    }
+  }
 
 
 }
+
+# devRateFind(orderSP = "Coleoptera")
