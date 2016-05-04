6a746c7ef6021dffddcac1c4037f41796a8d059d
Francois Rebaudo <francois.rebaudo@ird.fr>
2016-05-04 12:16:35 -0400

    man

--- R/devRate_info.R
+++ R/devRate_info.R
@@ -24,34 +24,60 @@ devRateInfo <- function(eq){
 #' @param eq The name of the equation.
 #' @param sortBy The filter to seperate species ("ordersp", "family"sp, "sp").
 #' @return Nothing.
+#' @examples
+#' devRatePlotInfo(eq = campbell_74)
 #' @export
 devRatePlotInfo <- function(eq, sortBy = "sp", ...){
   listPlot <- split(eq$startVal, eq$startVal[sortBy])
   plot(0, type = "n", xlab = "Temperature", ylab = "Developmental rate", ...)
+  x <- seq(from = 0, to = 50, length.out = 100)
   for(i in 1:length(listPlot)){
     for(j in 1:nrow(listPlot[[i]])){
       switch(EXPR = eq$id,
+             "eq010" = {},
+             "eq020" = {},
              "eq030" = {
                aa <- listPlot[[i]][j, 5]
                bb <- listPlot[[i]][j, 6]
-               x <- seq(from = 0, to = 50, length.out = 100)
-               # rT <- aa + bb * T
-               # lines(x = T, y = rT, col = i)
                fx <- as.function(alist(x=, eval(parse(text=eq$eqAlt))))
                curve(fx, add = TRUE, col = i)
              },
-             {}
+             "eq040" = {},
+             "eq050" = {},
+             "eq060" = {},
+             "eq070" = {},
+             "eq080" = {},
+             "eq090" = {},
+             "eq100" = {},
+             "eq110" = {},
+             "eq120" = {},
+             "eq130" = {},
+             "eq140" = {},
+             "eq150" = {},
+             "eq160" = {},
+             "eq170" = {},
+             "eq180" = {},
+             "eq190" = {},
+             "eq200" = {},
+             "eq210" = {},
+             "eq220" = {},
+             {
+              # otherwise
+             }
       )
     }
   }
   legend("topleft", legend = names(listPlot), col = 1:length(listPlot), lwd = 1)
 }
 
+
+
+### testing area
 # for(i in names(devRateEqList)){devRateInfo(eq = get(i))}
-
-
-devRatePlotInfo(eq = campbell_74, sortBy = "ordersp", xlim = c(0,30), ylim = c(0,0.2))
-devRatePlotInfo(eq = campbell_74, sortBy = "familysp", xlim = c(0,30), ylim = c(0,0.2))
+# for(i in names(devRateEqList)){devRatePlotInfo(eq = get(i), sortBy = "ordersp", xlim = c(0,50), ylim = c(0,0.2))}
+#
+# devRatePlotInfo(eq = campbell_74, sortBy = "ordersp", xlim = c(0,30), ylim = c(0,0.2))
+# devRatePlotInfo(eq = campbell_74, sortBy = "familysp", xlim = c(0,30), ylim = c(0,0.2))
 
 
 
