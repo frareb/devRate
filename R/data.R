95168c426540362087b59503128aeb2820e6fd18
frareb <francois.rebaudo@ird.fr>
2016-05-24 14:08:34 -0400

    init commit

---
+++ R/data.R
@@ -0,0 +1,445 @@
+#' Janisch equation of developmental rate as a function of temperature.
+#'
+#' Janisch, E. (1932) The influence of temperature on the life-history of insects.
+#' Transactions of the Royal Entomological Society of London 80(2): 137-68.
+#' Analytis, S. (1977) Uber die Relation zwischen biologischer Entwicklung und
+#' Temperatur bei phytopathogenen Pilzen. Journal of Phytopathology 90(1): 64-76.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"janisch_32"
+
+#' Davidson equation of developmental rate as a function of temperature.
+#'
+#' Davidson, J. (1944). On the relationship between temperature and rate of development of insects
+#' at constant temperatures. The Journal of Animal Ecology:26-38.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"davidson_44"
+
+#' Campbell et al. equation of developmental rate as a function of temperature.
+#'
+#' Campbell, A., Frazer, B. D., Gilbert, N. G. A. P., Gutierrez, A. P., & Mackauer, M. (1974).
+#' Temperature requirements of some aphids and their parasites. Journal of applied ecology, 431-438.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/10.2307/2402197}
+"campbell_74"
+
+#' Stinner et al equation of developmental rate as a function of temperature.
+#'
+#' Stinner, R., Gutierrez, A. & Butler, G. (1974) An algorithm for temperature-dependent growth
+#' rate simulation. The Canadian Entomologist, 106, 519-524.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"stinner_74"
+
+#' Logan et al. equation 6 of developmental rate as a function of temperature.
+#'
+#' Logan, J. A., Wollkind, D. J., Hoyt, S. C., and Tanigoshi, L. K. (1976). An analytic model
+#' for description of temperature dependent rate phenomena in arthropods. Environmental
+#' Entomology, 5(6), 1133-1140.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"logan6_76"
+
+#' Logan et al. equation 10 of developmental rate as a function of temperature.
+#'
+#' Logan, J. A., Wollkind, D. J., Hoyt, S. C., and Tanigoshi, L. K. (1976). An analytic model
+#' for description of temperature dependent rate phenomena in arthropods. Environmental
+#' Entomology, 5(6), 1133-1140.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"logan10_76"
+
+#' Sharpe and DeMichele equation of developmental rate as a function of temperature.
+#'
+#' Sharpe, P.J. & DeMichele, D.W. (1977) Reaction kinetics of poikilotherm development.
+#' Journal of Theoretical Biology, 64, 649-670.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"sharpeDeMichele_77"
+
+#' Analytis equation of developmental rate as a function of temperature.
+#'
+#' Analytis, S. (1977) Uber die Relation zwischen biologischer Entwicklung und Temperatur bei
+#' phytopathogenen Pilzen. Journal of Phytopathology 90(1): 64-76.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"analytis_77"
+
+#' Schoolfield et al. equation of developmental rate as a function of temperature.
+#'
+#' Schoolfield, R., Sharpe, P. & Magnuson, C. (1981) Non-linear regression of biological
+#' temperature-dependent rate models based on absolute reaction-rate theory.
+#' Journal of theoretical biology, 88, 719-731.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"schoolfield_81"
+
+#' Taylor equation of developmental rate as a function of temperature.
+#'
+#' Taylor, F. (1981) Ecology and evolution of physiological time in insects.
+#' American Naturalist, 1-23.
+#' Lamb, RJ. (1992) Developmental rate of Acyrthosiphon pisum (Homoptera: Aphididae) at low
+#' temperatures: implications for estimating rate parameters for insects.
+#' Environmental Entomology 21(1): 10-19.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"taylor_81"
+
+#'  Second-order polynomial equation of developmental rate as a function of temperature.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+"poly2"
+
+#' Harcourt and Yee equation of developmental rate as a function of temperature.
+#'
+#' Harcourt, D. and Yee, J. (1982) Polynomial algorithm for predicting the duration of insect
+#' life stages. Environmental Entomology, 11, 581-584.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"harcourtYee_82"
+
+#' Fourth-order polynomial equation of developmental rate as a function of temperature.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+"poly4"
+
+#' Holling type III equation of developmental rate as a function of temperature.
+#'
+#' Hilbert, DW, y JA Logan (1983) Empirical model of nymphal development for the migratory
+#' grasshopper, Melanoplus sanguinipes (Orthoptera: Acrididae).
+#' Environmental Entomology 12(1): 1-5.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"hilbertLogan_83"
+
+#' Lamb equation of developmental rate as a function of temperature.
+#'
+#' Lamb, R. J., Gerber, G. H., & Atkinson, G. F. (1984). Comparison of developmental rate curves
+#' applied to egg hatching data of Entomoscelis americana Brown (Coleoptera: Chrysomelidae).
+#' Environmental entomology, 13(3), 868-872.
+#' Lamb, RJ. (1992) Developmental rate of Acyrthosiphon pisum (Homoptera: Aphididae) at low
+#' temperatures: implications for estimating rate parameters for insects.
+#' Environmental Entomology 21(1): 10-19.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"lamb_92"
+
+#' Lactin et al. equation 1 of developmental rate as a function of temperature.
+#'
+#' Lactin, Derek J, NJ Holliday, DL Johnson, y R Craigen (995) Improved rate model of
+#' temperature-dependent development by arthropods. Environmental Entomology 24(1): 68-75.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"lactin1_95"
+
+#' Lactin et al. equation 2 of developmental rate as a function of temperature.
+#'
+#' Lactin, Derek J, NJ Holliday, DL Johnson, y R Craigen (995) Improved rate model of
+#' temperature-dependent development by arthropods. Environmental Entomology 24(1): 68-75.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"lactin2_95"
+
+#' Briere et al equation 1 of developmental rate as a function of temperature.
+#'
+#' Briere, J.F., Pracros, P., le Roux, A.Y. and Pierre, S. (1999) A novel rate model of
+#' temperature-dependent development for arthropods. Environmental Entomology, 28, 22-29.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"briere1_99"
+
+#' Briere et al equation 2 of developmental rate as a function of temperature.
+#'
+#' Briere, J.F., Pracros, P., le Roux, A.Y. and Pierre, S. (1999) A novel rate model of
+#' temperature-dependent development for arthropods. Environmental Entomology, 28, 22-29.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"briere2_99"
+
+#' Kontodimas et al. equation of developmental rate as a function of temperature.
+#'
+#' Kontodimas, D.C., Eliopoulos, P.A., Stathas, G.J. and Economou, L.P. (2004) Comparative
+#' temperature-dependent development of Nephus includens (Kirsch) and Nephus bisignatus
+#' (Boheman)(Coleoptera: Coccinellidae) preying on Planococcus citri
+#' (Risso)(Homoptera: Pseudococcidae): evaluation of a linear and various nonlinear models
+#' using specific criteria. Environmental Entomology 33(1): 1-11.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"kontodimas_04"
+
+#' Simplified beta type equation of developmental rate as a function of temperature.
+#'
+#' Damos, P.T., and Savopoulou-Soultani, M. (2008). Temperature-dependent bionomics and modeling
+#' of Anarsia lineatella (Lepidoptera: Gelechiidae) in the laboratory.
+#' Journal of economic entomology, 101(5), 1557-1567.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"damos_08"
+
+#' Inverse second-order polynomial equation of developmental rate as a function of temperature.
+#'
+#' Damos, P., and Savopoulou-Soultani, M. (2011) Temperature-driven models for insect
+#' development and vital thermal requirements. Psyche: A Journal of Entomology, 2012.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"damos_11"
+
+#' Wang et al. equation of developmental rate as a function of temperature.
+#'
+#' Wang, R., Lan, Z. & Ding, Y. (1982) Studies on mathematical models of the relationship
+#' between insect development and temperature. Acta Ecol. Sin, 2, 47–57.
+#'
+#' @format A list of eight elements describing the equation.
+#' \describe{
+#'   \item{eq}{The equation as a formula object.}
+#'   \item{eqAlt}{The equation as a string.}
+#'   \item{name}{The equation name.}
+#'   \item{ref}{The equation reference.}
+#'   \item{refShort}{The equation reference shortened.}
+#'   \item{startVal}{The parameter values found in the literature with their references.}
+#'   \item{com}{An otional comment about the equation use.}
+#'   \item{id}{An id to identificate the equation.}
+#' }
+#' @source \url{https://www.doi.org/XXX}
+"wang_82"
