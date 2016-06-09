c5c9ed0c4839eb9fede5a9bd42adce72f33b0bb0
Francois Rebaudo <francois.rebaudo@ird.fr>
2016-06-09 18:52:22 -0400

    without function yet

--- R/devRate_pheno.R
+++ R/devRate_pheno.R
@@ -1,29 +1,59 @@
-devRateIBM <- function(tempTS, timeStepTS, models, numInd = 1000, stocha){
+devRateIBM <- function(tempTS, timeStepTS, models, numInd = 100, stocha, timeLayEggs = 1){
 
 }
 
 
-tempTS <- rnorm(n = 50, mean = 25, sd = 1)
-timeStepTS <- 2
+tempTS <- rnorm(n = 100, mean = 25, sd = 1)
+timeStepTS <- 1
 models <- list(mEggs, mLarva, mPupa)
+timeLayEggs <- 1
+stocha <- 0.05
+numInd <- 100
 
+for(ind in 1:numInd){
+  g <- 0
+  tx <- 0
+  while(tx < length(tempTS)){
+    g <- g + 1
+    for(i in seq_along(models)){
+      currentDev <- 0
+      while(currentDev < 1){
+        tx <- tx + 1
+        if(tx > length(tempTS)){break}
+        currentDev <- currentDev + rnorm(n = 1, mean = stats::predict(models[[i]], newdata = list(T = tempTS[tx])), sd = stocha) * timeStepTS
+      }
+      # if(g < 10){gg <- paste0("0", g)}else{gg <- as.character(g)}
+      # if(i < 10){ii <- paste0("0", i)}else{ii <- as.character(i)}
+      assign(paste0("g", g, "s", i), tx)
+    }
+    tx <- tx + as.integer(timeLayEggs)
+  }
+  stages <- paste0(rep(paste0("g",1:(g + 3)), each = length(models)),"s", 1:length(models))
+  if(exists(x = "communityInd")){
+    # stages <- c(stages[1:((g - 1) * length(models) + (i - 1))], rep(NA, (ncol(communityInd) - ((g - 1) * length(models) + (i - 1)))))
+    stages <- c(stages[1:(((g - 1) * length(models) + (i - 1)) - 1)], rep(NA, ((ncol(communityInd) - ((g - 1) * length(models) + (i - 1))) + 1)))
+  }else{
+    # stages <- c(stages[1:((g - 1) * length(models) + (i - 1))], rep(NA, (length(stages) - ((g - 1) * length(models) + (i - 1)))))
+    stages <- c(stages[1:(((g - 1) * length(models) + (i - 1)) - 1)], rep(NA, ((length(stages) - ((g - 1) * length(models) + (i - 1))) + 1)))
+  }
+  currentInd <- sapply(stages, function(k){
+    if(!is.na(k)){return(get(k))}else{return(NA)}
+  })
+  if(exists(x = "communityInd")){
+    communityInd <- rbind(communityInd, currentInd)
+  }else{
+    communityInd <- matrix(currentInd, ncol = length(currentInd))
+  }
+}
+bdd <- communityInd
+rm(communityInd)
 
+plot(0, type = 'n', xlim = c(0, length(tempTS)), ylim = c(0, 0.5))
+colG <- rep(1:8, each = length(models))
+ltyG <- rep(1:length(models), 8)
+for(j in 1:ncol(bdd)){
+  points(density(bdd[,j], na.rm = TRUE, adjust = 2), type ='l', col = colG[j], lty = ltyG[j], lwd = 2)
+}
 
+numGen <- sum(!is.na(apply(bdd, MARGIN = 2, FUN = mean, na.rm = TRUE))) / length(models)
 
-tx <- 0
-G01S01 <- cumsum(stats::predict(models[[1]], newdata = list(T = tempTS)) * timeStepTS)
-G01S01 <- G01S01[G01S01 <= 1]
-tx <- tx + length(G01S01) + 1
-tG01S01 <- tx
-
-G01S02 <- cumsum(stats::predict(models[[2]], newdata = list(T = tempTS[tx:length(tempTS)])) * timeStepTS)
-G01S02 <- G01S02[G01S02 <= 1]
-tx <- tx + length(G01S02) + 1
-tG01S02 <- tx
-
-G01S03 <- cumsum(stats::predict(models[[3]], newdata = list(T = tempTS[tx:length(tempTS)])) * timeStepTS)
-G01S03 <- G01S03[G01S03 <= 1]
-tx <- tx + length(G01S03) + 1
-tG01S03 <- tx
-
-plot(x = c(tG01S01, tG01S02,tG01S03), y = c(1,1,1), xlim = c(0, 50))
