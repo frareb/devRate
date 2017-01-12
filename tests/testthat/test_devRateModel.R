8a615ab6b1ff1e504a6e3beb4b9c049e26e426b9
Francois Rebaudo <francois.rebaudo@ird.fr>
2017-01-12 17:33:27 -0400

    R1Q5: testing units (progress 30/35)

--- tests/testthat/test_devRateModel.R
+++ tests/testthat/test_devRateModel.R
@@ -261,38 +261,254 @@ test_that("devRateModel harcourtYee_82 Hypera postica (Coleoptera:Curculionidae)
   expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
 })
 
-# aa = 19.43
-# bb = 10490
-# cc = -156.9
-# dd = -44373
-# ff = 226.6
-# gg = 69113
-# yy <- rnorm(n = length(xx), mean = eval({x = xx; parse(text = taylor_81$eqAlt[1])}), sd = 0.01)
-# yy[yy < 0] <- 0
-# plot(xx, yy)
+test_that("devRateModel poly4 Tuta absoluta (Lepidoptera:Gelechiidae)", {
+  xx <- seq(from = 10, to = 50, by = 1)
+  yy <- c(0.00000000, 0.01740821, 0.01220815, 0.01662167, 0.02894427, 0.02778375,
+          0.04298098, 0.06337377, 0.04259963, 0.04382139, 0.05071260, 0.05552906,
+          0.08036814, 0.06739671, 0.09720684, 0.08217325, 0.10354591, 0.09996859,
+          0.11375681, 0.11507291, 0.14883151, 0.13347678, 0.14945011, 0.17115580,
+          0.16503220, 0.18094458, 0.15573351, 0.17484868, 0.18130818, 0.18935105,
+          0.18944839, 0.19123078, 0.17856466, 0.15369201, 0.14431060, 0.13409134,
+          0.10092687, 0.05206789, 0.02728394, 0.00000000, 0.00000000)
+  testNLS <- devRateModel(eq = poly4, temp = xx, devRate = yy,
+                          startValues = list(a0 = -0.27, a1 = 0.049, a2 = -0.00310, a3 = 0.000094, a4 = -1.00e-06))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel rootsq_82 Symmetrischema tangolias (Lepidoptera:Gelechiidae)", {
+  xx <- seq(from = 10, to = 50, by = 1)
+  yy <- c(0.05076648, 0.04130829, 0.03100395, 0.05420404, 0.07027619, 0.09081254,
+          0.07973797, 0.09178988, 0.09120504, 0.10319485, 0.12150123, 0.13037226,
+          0.15253398, 0.14760324, 0.17405035, 0.17703873, 0.18051293, 0.20000342,
+          0.22038633, 0.21933672, 0.24568362, 0.27005526, 0.27972188, 0.29105867,
+          0.31291853, 0.33053615, 0.35858438, 0.36573842, 0.37366271, 0.40343608,
+          0.41379410, 0.44065095, 0.46758776, 0.47282415, 0.48604381, 0.52820252,
+          0.55619338, 0.56618472, 0.58957850, 0.62538123, 0.64705128)
+  testNLS <- devRateModel(eq = rootsq_82, temp = xx, devRate = yy,
+                          startValues = list(bb = -0.0151, Tb = -2.8397))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel hilbertLogan_83 Melanoplus sanguinipes (Orthoptera:Acrididae)", {
+  xx <- seq(from = 8, to = 45, by = 1)
+  yy <- c(0.0011014477, 0.0012507442, 0.0001167703, 0.0003532791, 0.0004263743,
+          0.0015807307, 0.0025007541, 0.0024451171, 0.0038479143, 0.0064245233,
+          0.0069100382, 0.0097557497, 0.0119485173, 0.0142912696, 0.0149056976,
+          0.0173977196, 0.0193806360, 0.0212603286, 0.0246371582, 0.0260012154,
+          0.0294022145, 0.0309236323, 0.0339868565, 0.0357495426, 0.0384880874,
+          0.0402338705, 0.0431885192, 0.0472998302, 0.0478493169, 0.0506640531,
+          0.0527050289, 0.0516846788, 0.0551387014, 0.0556545999, 0.0562771387,
+          0.0552666161, 0.0521032164, 0.0477044579)
+  testNLS <- devRateModel(eq = hilbertLogan_83, temp = xx, devRate = yy,
+                          startValues = list(phi = 0.2676, aa = 58.6200, Tb = 7.6570, Tmax = 45.0200, deltaT = 3.4330))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel wagner_88 Rhyzopertha dominica (Coleoptera:Bostrichidae)", {
+  xx <- seq(from = 0, to = 40, by = 1)
+  yy <- c(0.0022212491, 0.0019379943, 0.0046610670, 0.0038109754, 0.0034486857,
+          0.0047077040, 0.0029604436, 0.0052875661, 0.0053446159, 0.0063670157,
+          0.0061276112, 0.0060130713, 0.0070109376, 0.0053224850, 0.0072778832,
+          0.0093596225, 0.0100493711, 0.0102531971, 0.0103169375, 0.0096810912,
+          0.0115920321, 0.0135770996, 0.0140990536, 0.0155335820, 0.0154300307,
+          0.0179327532, 0.0178314388, 0.0208542864, 0.0196719180, 0.0210781947,
+          0.0242664001, 0.0265959275, 0.0260000039, 0.0283018708, 0.0310243408,
+          0.0333888139, 0.0270995388, 0.0002436405, 0.0000000000, 0.0000000000,
+          0.0022229075)
+  testNLS <- devRateModel(eq = wagner_88, temp = xx, devRate = yy,
+                          startValues = list(aa = 0.017, bb = 11172, cc = 952361, dd = 309.4))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel lamb_92 Entomoscelis americana (Coleoptera:Chrysomelidae)", {
+  xx <- seq(from = 4, to = 35, by = 1)
+  yy <- c(0.0011177457, 0.0000000000, 0.0027744302, 0.0004460741, 0.0020882305,
+          0.0048659816, 0.0046385278, 0.0083962283, 0.0089383489, 0.0076518397,
+          0.0109503542, 0.0111564471, 0.0150364540, 0.0165235404, 0.0189938734,
+          0.0238332060, 0.0229612488, 0.0271481105, 0.0292819680, 0.0346557231,
+          0.0360398266, 0.0397014187, 0.0413783578, 0.0439369633, 0.0481031628,
+          0.0489109593, 0.0515116666, 0.0539285523, 0.0533571975, 0.0552395531,
+          0.0552867174, 0.0534671900)
+  testNLS <- devRateModel(eq = lamb_92, temp = xx, devRate = yy,
+                          startValues = list(list(Rm = 0.05393, Tmax = 33.4000, To = 10.7500), list(T1 = 3.377000)))
+  expect_is(testNLS[[1]], "nls")
+  expect_is(testNLS[[2]], "nls")
+  expect_gte(stats::cor(yy[1:31], stats::predict(testNLS[[1]])), 0.90)
+  expect_gte(stats::cor(yy[31:32], stats::predict(testNLS[[2]])), 0.90)
+})
+
+test_that("devRateModel lactin1_95 Plutella xylostella (Lepidoptera:Yponomeutidae)", {
+  xx <- seq(from = 0, to = 35, by = 1)
+  yy <- c(0.1822941, 0.2219479, 0.2255983, 0.2449956, 0.3109943, 0.3653696,
+          0.4123151, 0.4554988, 0.5404501, 0.6054672, 0.7141738, 0.7866237,
+          0.9141232, 1.0533414, 1.1567953, 1.3182321, 1.5054745, 1.6934138,
+          1.8920394, 2.1144703, 2.3537863, 2.6178237, 2.8850753, 3.1874894,
+          3.4406528, 3.7307796, 4.0232947, 4.2308143, 4.4163537, 4.5410291,
+          4.4970305, 4.2682834, 3.8589188, 3.0975230, 1.9257092, 0.2139277)
+  testNLS <- devRateModel(eq = lactin1_95, temp = xx, devRate = yy,
+                          startValues = list(aa = 0.170000, Tmax = 35.10000, deltaT = 5.700000))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel lactin2_95 Leptinotarsa decemlineata (Coleoptera:Chrysomelidae)", {
+  xx <- seq(from = 0, to = 38, by = 1)
+  yy <- c(0.000000000, 0.000000000, 0.001150229, 0.000000000, 0.010329007, 0.002114154,
+          0.012467492, 0.011841950, 0.020890812, 0.025269013, 0.025211605, 0.022588623,
+          0.053243659, 0.059586772, 0.066315001, 0.062347109, 0.072354588, 0.072628676,
+          0.089246183, 0.127517415, 0.118350089, 0.125706150, 0.136130564, 0.172161385,
+          0.163746042, 0.194455160, 0.205405549, 0.200541211, 0.242366873, 0.248637282,
+          0.264764651, 0.269341417, 0.263910144, 0.254994983, 0.265915585, 0.220850779,
+          0.180261610, 0.134250223, 0.063380383)
+  testNLS <- devRateModel(eq = lactin2_95, temp = xx, devRate = yy,
+                          startValues = list(aa = 0.139034, Tmax = 38.89003, deltaT = 7.167110, bb = -0.026410))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel beta_95 NA", {
+  xx <- seq(from = 15, to = 35, by = 1)
+  yy <- c(0.001417876, 0.005636010, 0.013581678, 0.019082356, 0.026517216,
+          0.032528973, 0.038774090, 0.044050438, 0.048498157, 0.056179586,
+          0.058087298, 0.062558419, 0.065349262, 0.067477422, 0.066865668,
+          0.069423345, 0.067166592, 0.066033683, 0.063193324, 0.055528744,
+          0.046781069)
+  testNLS <- devRateModel(eq = beta_95, temp = xx, devRate = yy,
+                          startValues = list(mu = -6.484, Tb = 14.8, aa = 1.071, Tc = 36.6, bb = 0.469))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel wangengel_98 NA", {
+  xx <- seq(from = 5, to = 35, by = 1)
+  yy <- c(0.1885586, 0.2447508, 0.2675420, 0.3352446, 0.3836423, 0.4204840,
+          0.5131225, 0.5717431, 0.5975940, 0.6562028, 0.7149159, 0.7541717,
+          0.7938599, 0.8322312, 0.8853133, 0.9172352, 0.9438028, 0.9536710,
+          0.9751491, 0.9954896, 0.9969244, 1.0128322, 0.9841107, 0.9726438,
+          0.9411715, 0.9080856, 0.8694295, 0.7953146, 0.7258664, 0.6700754,
+          0.5787964)
+  testNLS <- devRateModel(eq = wangengel_98, temp = xx, devRate = yy,
+                          startValues = list(Tmin = 0, aa = 1.5, Topt = 25))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel briere1_99 Cydia pomonella (Lepidoptera:Tortricidae)", {
+  xx <- seq(from = 5, to = 35, by = 1)
+  yy <- c(0.0000000000, 0.0000000000, 0.0000000000, 0.0000000000, 0.0000000000,
+          0.0008276912, 0.0024161151, 0.0038645223, 0.0043578848, 0.0072354110,
+          0.0072349070, 0.0098385738, 0.0108333796, 0.0125452909, 0.0148372325,
+          0.0174393359, 0.0190593441, 0.0200491951, 0.0224374464, 0.0245829079,
+          0.0278148202, 0.0264121875, 0.0279165289, 0.0303138541, 0.0306765425,
+          0.0287505897, 0.0324110598, 0.0320097185, 0.0310495736, 0.0271481739,
+          0.0223007786)
+  testNLS <- devRateModel(eq = briere1_99, temp = xx, devRate = yy,
+                          startValues = list(aa = 2.000e-05, Tmin = 9.6188, Tmax = 36.5508))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel briere2_99 Cydia pomonella (Lepidoptera:Tortricidae)", {
+  xx <- seq(from = 10, to = 30, by = 1)
+  yy <- c(0.003592012, 0.002441060, 0.005466671, 0.004373278, 0.005762749, 0.009782370,
+          0.009475021, 0.011327031, 0.012687551, 0.015284688, 0.014736900, 0.018212742,
+          0.019424187, 0.022882716, 0.024458473, 0.025876630, 0.027911330, 0.027726436,
+          0.029162758, 0.033745400, 0.035172396)
+
+  testNLS <- devRateModel(eq = briere2_99, temp = xx, devRate = yy,
+                          startValues = list(aa = 0.0000340, Tmin = 7.7659, Tmax = 34.1260, bb = 3.8342))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel bayoh_03 Anopheles gambiae (Diptera:Culicidae)", {
+  xx <- seq(from = 16, to = 34, by = 1)
+  yy <- c(0.00000000, 0.02360451, 0.03679854, 0.04256779, 0.05106298,
+          0.05494146, 0.05910256, 0.06544544, 0.07125289, 0.07310096,
+          0.07760755, 0.08423758, 0.08910340, 0.09361966, 0.09687718,
+          0.09940690, 0.09378216, 0.06982750, 0.00000000)
+  testNLS <- devRateModel(eq = bayoh_03, temp = xx, devRate = yy,
+                          startValues = list(aa = -0.05, bb = 0.005, cc = -2.139e-16, dd = -281357.7))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel kontodimas_04 Cydia pomonella (Lepidoptera:Tortricidae)", {
+  xx <- seq(from = 10, to = 40, by = 1)
+  yy <- c(0.0006747102, 0.0027825987, 0.0033743047, 0.0047231486, 0.0064218586,
+          0.0081886365, 0.0097703062, 0.0125784934, 0.0145758820, 0.0159444966,
+          0.0183775852, 0.0206127228, 0.0245370442, 0.0266028103, 0.0291879954,
+          0.0305220770, 0.0312787956, 0.0321701920, 0.0364631361, 0.0353332002,
+          0.0392814342, 0.0368932876, 0.0374896984, 0.0385335372, 0.0372273938,
+          0.0367543093, 0.0336959171, 0.0321736453, 0.0266896329, 0.0227111416,
+          0.0190010898)
+  testNLS <- devRateModel(eq = kontodimas_04, temp = xx, devRate = yy,
+                          startValues = list(aa = 5.7e-06, Tmin = 7.9376, Tmax = 43.3850))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel damos_08 Anarsia lineatella (Lepidoptera:Gelechiidae)", {
+  xx <- seq(from = 5, to = 35, by = 1)
+  yy <- c(0.000000000, 0.000000000, 0.013844075, 0.000000000, 0.009361758,
+          0.001454269, 0.000000000, 0.013183609, 0.000000000, 0.004798529,
+          0.014066903, 0.003628415, 0.009424874, 0.017446812, 0.010623413,
+          0.031509888, 0.033779900, 0.030489820, 0.023386860, 0.027721306,
+          0.049432465, 0.051402218, 0.054979934, 0.057700327, 0.060598896,
+          0.065206405, 0.061546110, 0.055924618, 0.057930173, 0.055963462,
+          0.031124983)
+  testNLS <- devRateModel(eq = damos_08, temp = xx, devRate = yy,
+                          startValues = list(aa = 3e-04, bb = 3.8297, cc = 4.876))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+test_that("devRateModel damos_11 NA", {
+  xx <- seq(from = 0, to = 35, by = 1)
+  yy <- c(0.08239590, 0.08072370, 0.10070833, 0.08878272, 0.13047990, 0.12654245,
+          0.11403194, 0.11003217, 0.11405621, 0.13394459, 0.12004746, 0.14363508,
+          0.13795026, 0.16381752, 0.15539005, 0.17926441, 0.15370672, 0.16926040,
+          0.18565617, 0.20515092, 0.20139358, 0.19017785, 0.21421971, 0.21199808,
+          0.22879180, 0.23448626, 0.20004777, 0.21789319, 0.20348710, 0.21354517,
+          0.21559372, 0.21210905, 0.17523718, 0.18001217, 0.18948697, 0.17359905)
+  testNLS <- devRateModel(eq = damos_11, temp = xx, devRate = yy,
+                          startValues = list(aa = 0.08, bb = -0.05, cc = 0.001))
+  expect_is(testNLS, "nls")
+  expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+})
+
+
+
 
 #
-# poly4
-# ratkowsky_82
-# rootsq_82
-# bieri1_83
-# hilbertLogan_83
-# wagner_88
-# lamb_92
-# lactin1_95
-# lactin2_95
-# beta_95
-# wangengel_98
-# briere1_99
-# briere2_99
-# bayoh_03
-# kontodimas_04
-# damos_08
-# damos_11
-# hansen_11
 # shi_11
 # perf2_11
 # regniere_12
+# ratkowsky_82
+# bieri1_83
+
+# test_that("devRateModel xxxx", {
+#   xx <- seq(from = 0, to = 35, by = 1)
+#   yy <- c()
+#
+#   aa = xxx
+#   bb = xxx
+#   Tm = xxx
+#   Tmin = xxx
+#   yy <- rnorm(n = length(xx), mean = eval({x = xx; parse(text = xxx$eqAlt[1])}), sd = 0.01)
+#   yy[yy < 0] <- 0
+#   plot(xx, yy)
+#
+#   testNLS <- devRateModel(eq = xxx, temp = xx, devRate = yy,
+#                           startValues = list(aa = xxx, bb = xxx, Tm = xxx, Tmin = xxx))
+#   expect_is(testNLS, "nls")
+#   expect_gte(stats::cor(yy, stats::predict(testNLS)), 0.90)
+# })
+
 
 
 
