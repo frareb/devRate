0c02811644bc14ef6b5a3669b95e6cf47e8c00c2
Francois Rebaudo <francois.rebaudo@ird.fr>
2017-07-31 16:56:50 +0200

    Revision3- AEQ5: @return

--- R/devRate_find.R
+++ R/devRate_find.R
@@ -3,7 +3,7 @@
 #' @param orderSP Find models by Order.
 #' @param familySP Find models by Family.
 #' @param species Find models by species (Genus species).
-#' @return Nothing.
+#' @return A data.frame with the name of the equation and the number of occurrences in the database.
 #' @examples
 #' devRateFind(orderSP = "Lepidoptera")
 #' devRateFind(familySP = "Gelechiidae")
@@ -11,26 +11,39 @@
 #' @export
 devRateFind <- function(orderSP = "", familySP = "", species = ""){
   devRateEqList <- devRateEqList # avoid "no visible binding for global variable devRateEqList" Note
-  # sum(sapply(devRateEqList, function(i){nrow(i$startVal[,1:5])})) # number of entries
+  vFind <- vector()
+  vEq <- vector()
   for(i in names(devRateEqList)){
     eq <- get(i)
     if(orderSP != "" & familySP == "" & species == ""){
       if(orderSP %in% eq$startVal[,"ordersp"] == TRUE){
         occu <- sum(as.character(eq$startVal[,"ordersp"]) == orderSP)
-        cat("\n[", i, "]  ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "]  ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
+        vFind <- c(vFind, occu)
+        vEq <- c(vEq, i)
       }
     }
     if(familySP != "" & species == ""){
       if(familySP %in% eq$startVal[,"familysp"] == TRUE){
         occu <- sum(as.character(eq$startVal[,"familysp"]) == familySP)
-        cat("\n[", i, "]  ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "]  ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
+        vFind <- c(vFind, occu)
+        vEq <- c(vEq, i)
       }
     }
     if(species != ""){
       if(species %in% eq$startVal[,"genSp"] == TRUE){
         occu <- sum(as.character(eq$startVal[,"genSp"]) == species)
-        cat("\n[", i, "] ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "] ", strwrap(x = paste0(eq$name, ": ", eq$refShort, " (", occu, " times)"), width = 80))
+        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
+        vFind <- c(vFind, occu)
+        vEq <- c(vEq, i)
       }
     }
   }
-}
+  dfFind <- data.frame(equation = vEq, occu = vFind)
+  dfFind <- dfFind[order(dfFind[, 2], decreasing = TRUE), ]
+  return(dfFind)
+} ## campbell_74$startVal[campbell_74$startVal["genSp"] == "Tuta absoluta",]
