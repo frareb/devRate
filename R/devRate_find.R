a3a930cf9510b032d3a566f099b977a2bc303e96
Francois Rebaudo <francois.rebaudo@ird.fr>
2017-10-12 11:37:43 +0200

    Revision4-R3Q6-7: parameters number Revision4-R3Q6-6: single value Revision4-R3Q6-6: del. == TRUE

--- R/devRate_find.R
+++ R/devRate_find.R
@@ -5,7 +5,8 @@
 #' @param orderSP Find models by Order.
 #' @param familySP Find models by Family.
 #' @param species Find models by species (Genus species).
-#' @return A data.frame with the name of the equations and the number of occurrences in the database.
+#' @return A data.frame with the name of the equations, the number of occurrences in the database,
+#'   and the number of parameters for each equation.
 #' @examples
 #' devRateFind(orderSP = "Lepidoptera")
 #' devRateFind(familySP = "Gelechiidae")
@@ -17,36 +18,44 @@
 #' @export
 devRateFind <- function(orderSP = "", familySP = "", species = ""){
   devRateEqList <- devRateEqList # avoid "no visible binding for global variable devRateEqList" Note
-  vFind <- vector()
-  vEq <- vector()
-  for(i in names(devRateEqList)){
-    eq <- get(i)
-    if(orderSP != "" & familySP == "" & species == ""){
-      if(orderSP %in% eq$startVal[,"ordersp"] == TRUE){
-        occu <- sum(as.character(eq$startVal[,"ordersp"]) == orderSP)
-        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
-        vFind <- c(vFind, occu)
-        vEq <- c(vEq, i)
+  if(length(c(orderSP, familySP, species)) == 3 &&
+     is.character(orderSP) &&
+     is.character(familySP) &&
+     is.character(species)){
+    vFind <- vector()
+    vEq <- vector()
+    for(i in names(devRateEqList)){
+      eq <- get(i)
+      if(orderSP != "" & familySP == "" & species == ""){
+        if(orderSP %in% eq$startVal[,"ordersp"]){ #  == TRUE
+          occu <- sum(as.character(eq$startVal[,"ordersp"]) == orderSP)
+          vFind <- c(vFind, occu)
+          vEq <- c(vEq, i)
+        }
+      }
+      if(familySP != "" & species == ""){
+        if(familySP %in% eq$startVal[,"familysp"]){ # == TRUE
+          occu <- sum(as.character(eq$startVal[,"familysp"]) == familySP)
+          vFind <- c(vFind, occu)
+          vEq <- c(vEq, i)
+        }
+      }
+      if(species != ""){
+        if(species %in% eq$startVal[,"genSp"]){ # == TRUE
+          occu <- sum(as.character(eq$startVal[,"genSp"]) == species)
+          vFind <- c(vFind, occu)
+          vEq <- c(vEq, i)
+        }
       }
     }
-    if(familySP != "" & species == ""){
-      if(familySP %in% eq$startVal[,"familysp"] == TRUE){
-        occu <- sum(as.character(eq$startVal[,"familysp"]) == familySP)
-        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
-        vFind <- c(vFind, occu)
-        vEq <- c(vEq, i)
-      }
-    }
-    if(species != ""){
-      if(species %in% eq$startVal[,"genSp"] == TRUE){
-        occu <- sum(as.character(eq$startVal[,"genSp"]) == species)
-        # cat("\n[", i, "]", strwrap(x = paste0(": ", occu, " times"), width = 80))
-        vFind <- c(vFind, occu)
-        vEq <- c(vEq, i)
-      }
-    }
+    vParam <- sapply(vEq, function(j){
+      sum(grepl(names(get(j)$startVal), pattern = "^(param)"))
+    })
+    dfFind <- data.frame(equation = vEq, occu = vFind, paramNumb = vParam)
+    dfFind <- dfFind[order(dfFind[, 2], decreasing = TRUE), ]
+    rownames(dfFind) <- NULL
+    return(dfFind)
+  } else {
+    print("Error in arguments provided: only one argument of type character is allowed")
   }
-  dfFind <- data.frame(equation = vEq, occu = vFind)
-  dfFind <- dfFind[order(dfFind[, 2], decreasing = TRUE), ]
-  return(dfFind)
 }
