60bac8f83a8a86d6665ffa008dbad2e0f2752bdc
Francois Rebaudo <francois.rebaudo@ird.fr>
2020-01-31 10:38:24 +0100

    up coverage

--- tests/testthat/test_devRate_info.R
+++ tests/testthat/test_devRate_info.R
@@ -1,22 +1,41 @@
 # ------------------------------------------------------------------------------
 # Tests for devRateInfo
 # ------------------------------------------------------------------------------
-test_that("print devRateModel",{
+test_that("devRateInfo returns NULL",{
   res <- devRateInfo(eq = taylor_81)
   expect_equal(
     object = res,
     expected = NULL
   )
 })
+
 # ------------------------------------------------------------------------------
-# Tests for XXXX
+# Tests for devRatePlotInfo
 # ------------------------------------------------------------------------------
 
+test_that("devRatePlotInfo returns NULL",{
+  eqOpt <- devRateEqList
 
 
+  trash <- lapply(eqOpt, function(j){
+    sortOpt <- c("ordersp", "familysp", "genussp", "species", "genSp")
+    res <- lapply(sortOpt, function(i){
+      devRatePlotInfo(
+        eq = j,
+        sortBy = i,
+        xlim = c(0, 40),
+        ylim = c(0, 0.05)
+      )
+    })
+    sapply(res, function(i){
+      expect_equal(
+        object = i,
+        expected = NULL
+      )
+    })
+  })
 
-
-
+})
 
 
 # devtools::test()
