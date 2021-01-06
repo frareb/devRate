2152f547b480caba7ba2b31c430486bc7cfeb755
frareb <francois.rebaudo@ird.fr>
2021-01-06 11:40:40 +0100

    update following devRateQlStat changes

--- tests/testthat/test_devRate_devRateModelAll.R
+++ tests/testthat/test_devRate_devRateModelAll.R
@@ -1,7 +1,8 @@
 test_that("devRateModelAll", {
   myDf <- exTropicalMoth$raw$larva
-  fitAll <- devRateModelAll(df = myDf,
-                            algo = "LM",
-                            control = list(maxiter = 500))
+  fitAll <- devRateModelAll(
+    dfData = myDf,
+    algo = "LM",
+    control = list(maxiter = 500))
   expect_is(fitAll, "list")
 })
